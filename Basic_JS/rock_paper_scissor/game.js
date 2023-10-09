fba749cbe1d65e15f8c9ffff0f3630e1007d3c0f
Marius Bakken Berg <marius.bakken.berg@gmail.com>
2023-10-09 21:21:14 +0200

    La til ett enkelt UI

--- Basic_JS/rock_paper_scissor/game.js
+++ Basic_JS/rock_paper_scissor/game.js
@@ -26,27 +26,44 @@ function playRound(playerSelec, computerSelec) {
 
 
 
-function game(){
-    let playerscore = 0
-    let computerScore = 0
-    for (let i = 0; i<5; i++){
-        let playerSelec = prompt("Rock, paper or scissor?");
-        let computerSelec = getComputerChoice();
-        if (playRound(playerSelec, computerSelec)){
-            console.log(`You win! ${playerSelec} beats ${computerSelec}`);
-            playerscore++
-        }
-        else if (!playRound(playerSelec, computerSelec)) {
-            console.log(`Computer wins! ${computerSelec} beats ${playerSelec}`)
-            computerScore++
-        }
-        else{
-            console.log("It's a tie! No points.")
-        }
-        console.log(`The score is PlayerScore:${playerscore} ComputerScore ${computerScore}`)
-    }
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let gameEnded = false; // Legg til en variabel for å spore om spillet er avsluttet
 
-    
+    const container = document.querySelector('#container');
+    const buttons = document.querySelectorAll('button');
+    const scoreLine = document.createElement('p');
+    const score = document.createElement('p');
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            if (!gameEnded) { // Sjekk om spillet allerede er avsluttet
+                let computerSelec = getComputerChoice();
+                let playerSelec = button.id;
+
+                if (playRound(playerSelec, computerSelec)) {
+                    scoreLine.textContent = `You win! ${playerSelec} beats ${computerSelec}`;
+                    playerScore++;
+                } else if (!playRound(playerSelec, computerSelec)) {
+                    scoreLine.textContent = `Computer wins! ${computerSelec} beats ${playerSelec}`;
+                    computerScore++;
+                } else {
+                    scoreLine.textContent = "It's a tie! No points.";
+                }
+
+                score.textContent = `The score is PlayerScore: ${playerScore} ComputerScore: ${computerScore}`;
+
+                if (playerScore === 5 || computerScore === 5) {
+                    gameEnded = true; // Marker spillet som avsluttet
+                    buttons.forEach(button => button.disabled = true); // Deaktiver knappene
+                }
+            }
+        });
+    });
+
+    container.appendChild(scoreLine);
+    container.appendChild(score);
 }
 
 game()
